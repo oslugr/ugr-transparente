72a2183aba72ec5f873140647de4b96818bd815f
Jaime <jtbenavente@gmail.com>
2014-02-12 13:54:31 +0100

    Añadidos los elementos de la plantilla neutra

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/index', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
